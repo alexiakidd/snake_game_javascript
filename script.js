9a5e3853d0275879e2f61d4e177db7897e3ea950
alexiakidd <alexiakidd.dev@gmail.com>
2020-04-12 20:52:23 -0300

    Correção de erro nas direções em iniciarJogo()

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0  && direction == "right") snake[0].y = 16 * box;
+    if(snake[0].y < 0  && direction == "up") snake[0].y = 16 * box;
 
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
@@ -72,7 +72,7 @@ function iniciarJogo(){
         food.x = Math.floor(Math.random() * 15 + 1) * box, 
         food.y = Math.floor(Math.random() * 15 + 1) * box
     }
-    
+
     let newHead = {
         x: snakeX,
         y: snakeY
